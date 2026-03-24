b0c00a6a1a9790a45f5105d386bcccd01341a61f
beastcode9999 <beastcode9999@gmail.com>
2026-03-24 11:40:17 +0930

    feat: add getCourse method to retrieve courses

--- tms_mobile_app_lite/lib/services/api_service.dart
+++ tms_mobile_app_lite/lib/services/api_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:tms_mobile_app_lite/services/token_service.dart';
 
 class ApiService {
   static const String baseUrl = 'http://10.0.2.2:8000/api';
@@ -21,6 +22,27 @@ class ApiService {
     } else {
       // else throw an exception
       throw Exception('Login failed');
+    }
+  }
+
+  // Get courses
+  static Future<List<dynamic>> getCourses() async {
+    final token = await TokenService.getAccessToken();
+
+    final response = await http.get(
+      Uri.parse('$baseUrl/courses/'),
+      headers: {
+        // tells Django to parse this as JSOn
+        'Content-Type': 'application/json',
+        // veriy token and if user is logged in
+        'Authorization': 'Bearer $token',
+      },
+    );
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } 
+    else {
+      throw Exception('Failed to fetch courses');
     }
   }
 }
